365be22d70740589db423480dbf839fbb4342e2a
Ervandogan12 <ervandogan12@gmail.com>
2020-04-23 16:18:50 +0300

    view yapısı tek klasör içerisine atıldı

---
+++ panel/application/views/dashboard_v/index.php
@@ -0,0 +1,39 @@
+<?php $this->load->helper('url'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php $this->load->view("includes/head"); ?>	
+</head>
+	
+<body class="menubar-left menubar-unfold menubar-light theme-primary">
+<!--============= start main area -->
+<?php $this->load->view("includes/navbar"); ?>
+
+<?php $this->load->view("includes/aside"); ?>
+<!-- navbar search -->
+<?php $this->load->view("includes/navbar_search"); ?>
+<!-- .navbar-search -->
+
+
+<!-- APP MAIN ==========-->
+<main id="app-main" class="app-main">
+  
+  <div class="wrap">
+	<section class="app-content">
+		
+<?php $this->load->view("dashboard_v/content") ?>
+		
+	</section><!-- #dash-content -->
+</div><!-- .wrap -->
+  <!-- APP FOOTER -->
+  <?php $this->load->view("includes/footer"); ?>
+  <!-- /#app-footer -->
+</main>
+<!--========== END app main -->
+
+
+<?php $this->load->view("includes/include_script"); ?>
+
+</body>
+</html>
