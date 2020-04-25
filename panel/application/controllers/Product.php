2a6313685806b4fbad9e2885b1ad3249f46b72d6
Ervandogan12 <ervandogan12@gmail.com>
2020-04-25 22:39:26 +0300

    uyarı mesajı tanımlandı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -61,7 +61,15 @@ class Product extends CI_Controller
 
         else{
 
-            echo validation_errors();
+           // echo validation_errors();
+
+        $viewData = new stdClass();
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+        $viewData->form_error = true;
+
+       $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
         }
     }
 
