a0e0c657c4c0e1fbad679175683944f72662e43c
Ervandogan12 <ervandogan12@gmail.com>
2020-04-26 04:06:01 +0300

    update yapıldı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -1,133 +1,201 @@
-    <?php
+<?php
 
-    class Product extends CI_Controller
+class Product extends CI_Controller
+{
+    public $viewFolder = "";
+
+    public function __construct()
     {
-        public $viewFolder = "";
 
-        public function __construct()
-        {
+        parent::__construct();
 
-            parent::__construct();
+        $this->viewFolder = "product_v";
 
-            $this->viewFolder = "product_v";
+        $this->load->model("product_model");
 
-            $this->load->model("product_model");
+    }
 
-        }
+    public function index(){
 
-        public function index(){
+        $viewData = new stdClass();
+
+        /** Tablodan Verilerin Getirilmesi.. */
+        $items = $this->product_model->get_all();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
+        $viewData->items = $items;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
+
+    public function new_form(){
+
+        $viewData = new stdClass();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+    }
+
+    public function save(){
+
+        $this->load->library("form_validation");
+
+        // Kurallar yazilir..
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
+            )
+        );
+
+        // Form Validation Calistirilir..
+        // TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        // Monitör Askısı
+        // monitor-askisi
+
+        if($validate){
+
+            $insert = $this->product_model->add(
+                array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSEO($this->input->post("title")),
+                    "rank"          => 0,
+                    "isActive"      => 1,
+                    "createdAt"     => date("Y-m-d H:i:s")
+                )
+            );
+
+            // TODO Alert sistemi eklenecek...
+            if($insert){
+
+                redirect(base_url("product"));
+
+            } else {
+
+                redirect(base_url("product"));
+
+            }
+
+        } else {
 
             $viewData = new stdClass();
-
-            /** Tablodan Verilerin Getirilmesi.. */
-            $items = $this->product_model->get_all(
-              array(
-
-
-              ));
 
             /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-            $viewData->viewFolder = $this->viewFolder;
-            $viewData->subViewFolder = "list";
-            $viewData->items = $items;
-
-            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-        }
-
-        public function new_form(){
-
-
-
-            $viewData = new stdClass();
-            $viewData->viewFolder = $this->viewFolder;
-            $viewData->subViewFolder = "add";
-            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-
-        }
-
-        public function save(){
-
-            $this->load->library("form_validation");
-
-            //kurallar yazılır
-            $this->form_validation->set_rules("title", "Başlık", "required|trim");
-
-            $this->form_validation->set_message(
-                array(
-                    "required" => "<b> {field}</b> alanı doldurulmalıdır")
-            );
-
-    //form validation çalıştırılır.true false döndürülür.
-
-            $validate = $this->form_validation->run();
-
-            //monitor askısı
-            //monitor-askisi
-
-            if ($validate) {
-
-              $insert = $this->product_model->add(
-                array(
-                   "title"=>$this->input->post("title"),
-                   "description"=>$this->input->post("description"),
-                   "url"=>convertToSEO($this->input->post("title")),
-                   "rank"=>0,
-                   "isActive"=>1,
-                   "createdAt"=> date("Y-m-d H:i:s")
-
-               )
-            );
-
-
-    //todo alert mesajları eklenecek...
-              if ($insert) {
-
-                  redirect(base_url("product"));
-
-              } else {
-
-                 echo "İşlem başarısız";
-                 redirect(base_url("product"));
-             }
-
-
-         }
-
-         else{
-
-               // echo validation_errors();
-
-            $viewData = new stdClass();
             $viewData->viewFolder = $this->viewFolder;
             $viewData->subViewFolder = "add";
             $viewData->form_error = true;
 
             $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-
         }
 
+        // Başarılı ise
+            // Kayit işlemi baslar
+        // Başarısız ise
+            // Hata ekranda gösterilir...
 
     }
 
     public function update_form($id){
 
+        $viewData = new stdClass();
 
-
-       $viewData = new stdClass();
-       $item = $this->product_model->get(
-        array(
-              "id"=> $id,
+        /** Tablodan Verilerin Getirilmesi.. */
+        $item = $this->product_model->get(
+            array(
+                "id"    => $id,
+            )
+        );
         
-        )
-
-      );
-
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
         $viewData->viewFolder = $this->viewFolder;
         $viewData->subViewFolder = "update";
-        $viewData->item=$item;
+        $viewData->item = $item;
+
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
 
-    }
-
 
     }
+
+    public function update($id){
+
+        $this->load->library("form_validation");
+
+        // Kurallar yazilir..
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
+            )
+        );
+
+        // Form Validation Calistirilir..
+        // TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        // Monitör Askısı
+        // monitor-askisi
+
+        if($validate){
+
+            $update = $this->product_model->update(
+                array(
+                    "id"    => $id
+                ),
+                array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSEO($this->input->post("title")),
+                )
+            );
+
+            // TODO Alert sistemi eklenecek...
+            if($update){
+
+                redirect(base_url("product"));
+
+            } else {
+
+                redirect(base_url("product"));
+
+            }
+
+        } else {
+
+            $viewData = new stdClass();
+
+            /** Tablodan Verilerin Getirilmesi.. */
+            $item = $this->product_model->get(
+                array(
+                    "id"    => $id,
+                )
+            );
+
+            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "update";
+            $viewData->form_error = true;
+            $viewData->item = $item;
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+
+        // Başarılı ise
+        // Kayit işlemi baslar
+        // Başarısız ise
+        // Hata ekranda gösterilir...
+
+    }
+
+}
